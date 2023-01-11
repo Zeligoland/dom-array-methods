52f964665f001c78402aed8ae6aeb6ec4c33ab47
Andrea Zapata <95andreazapata@gmail.com>
2023-01-11 17:15:51 -0500

    Using .reduce() for calculateWealth function

--- script.js
+++ script.js
@@ -37,18 +37,30 @@ function doubleMoney() {
   updateDOM();
 }
 
-// Sort users by richest
+// Sort users by richest with .sort()
 function sortByRichest() {
   data.sort((a, b) => b.money - a.money);
 
   updateDOM();
 }
 
-// Filter only millionaires
+// Filter only millionaires with .filter()
 function showMillionaires() {
-  data = data.filter(user => user.money > 1000000);
+  data = data.filter((user) => user.money > 1000000);
 
   updateDOM();
+}
+
+// Calculate entire wealth with .reduce()
+
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )} </strong><h3/>`;
+  main.appendChild(wealthEl);
 }
 
 // Add new obj to data arr
@@ -89,3 +101,4 @@ addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
 showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
